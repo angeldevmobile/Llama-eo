65937dd398cd81c76f733bf9607ed6a2bcfb07b2
Gabriel Zapata <zapata.axuariogabriel@gmail.com>
2025-07-23 21:14:17 -0500

    final changes

--- main.py
+++ main.py
@@ -10,7 +10,3 @@ app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(guru_routes)
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
